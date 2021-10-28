c2dc9e518060339def5547f00e2a9807bd2fb59f
Akmat <sirazhidinov.akmat@tftc.dev>
2021-10-28 14:31:02 +0600

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('show-text', {
+    props: ['msg'],
+    template: `<h2>{{ msg }}</h2>`
+})
+
+app.mount('#app')
